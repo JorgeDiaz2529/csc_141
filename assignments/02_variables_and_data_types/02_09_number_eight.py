a92c24f2d934fd5ed85e39d70d7bc68b6a452177
Jorge Diaz <jordiaz397@gmail.com>
2025-09-04 11:54:55 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_09_number_eight.py
@@ -0,0 +1,7 @@
+# Jorge Diaz
+# September 4, 2025
+# This programs prints out equations that always result in 8. 
+print(4+4)
+print(2*4)
+print(10-2)
+print(64/8)
---
+++ assignments/02_variables_and_data_types/02_10_favorite_number.py
@@ -0,0 +1,8 @@
+# Jorge Diaz
+# September 4, 2025
+# Prints out my favorite number
+favorite_number = 15
+print(f"My favorite number is {favorite_number}!")
+
+# 02_12_zen_of_python
+import this
